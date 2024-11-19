07e29dc8c719bf8607b62880d99c636e9a794288
AidanBennis <137786486+AidanBennis@users.noreply.github.com>
2024-11-19 21:34:48 +0000

    Update Apollo_Ephemeris.m
    made it work properly

--- Apollo_Ephemeris.m
+++ Apollo_Ephemeris.m
@@ -1,4 +1,4 @@
-function [kep,mass,M] = Apollo_Ephemeris(time)
+function [kep] = Apollo_Ephemeris(time)
 %Ephemeris of the Apollo Asteriod
 % OUTPUT
 %   kep     Keplerian parameters. It is a 6 entry vector:
@@ -9,7 +9,7 @@ function [kep,mass,M] = Apollo_Ephemeris(time)
 %               i is the inclination [rad];
 %               Om is the anomaly of the ascending node [rad];
 %               om is the anomaly of the pericentre [rad];
-%               MO is the true anomaly (from the pericentre) [rad].
+%               wom is the true anomaly (from the pericentre) [rad].
 %               time_Mo is the time at which the true anamoly ocurs [MJD]
 %
 %   mass    Mass of the NEO [kg]. It can be read from the database, or, if
@@ -30,11 +30,6 @@ om = 286.0329799312636*conv_rads;
 Mo = 252.868692344222*conv_rads;
 time_Mo = 60600; % 2024-Oct-17 in MJD
 
-kep = [a,e,i,Om,om,Mo];
-
-% Mass of Apollo
-mass = 2.00E+12;
-
 % True Anamoly From NEO Ephemeris does all this to get true Anamoly
 % at the time being simulated in Lambert
 n  = sqrt(mu_sun/a^3);
@@ -46,7 +41,8 @@ p = 2*pi*sqrt(a^3/mu_sun);
 np = floor(t/p);
 t = t-p*np;
 phi = n*t;
-M  = phi;
-if M > pi
-    M = M-2*pi;
-end
+ddf=(e*cos(phi)-1);
+phi=phi-t*n/ddf+(phi-e*sin(phi))/ddf;
+wom=2*atan(sqrt((1+e)/(1-e))*tan(phi*0.5));
+
+kep = [a,e,i,Om,om,wom];
