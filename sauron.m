7727796fd6f2fdf3ebda89f181d4216b77c685a7
samirahman1203 <157735830+samirahman1203@users.noreply.github.com>
2024-11-21 20:32:40 +0000

    Update sauron.m

--- sauron.m
+++ sauron.m
@@ -14,15 +14,15 @@ muE = 3.99e+5 ; % gravatational parameter of Earth
 rE = 42157; % Orbit around Earth in Km (6371 [radius] + 35786 [GTO])
 rE1 = 6571; % 200Km LEO Orbit for end of mission
 
-% Asteroid: Apollo
-a4 = 2.2e8; % Semi-major axis of Apollos orbit in km
-e4 = 0.559871597; % Eccentricity of Apollo orbit
-i4 = 6.352454347; % Inclination of Apollo orbit in degrees
-Omega4 = 35.55648326; % Right Ascension of Apollo orbit in degrees
-omega4 = 286.0329799; % Argument of periapsis of Apollo orbit in degrees
-M4 = 41.059574; % Mean anomaly of Apollo orbit in degrees
-rA = 5.775; % raduis of intial orbit around Apollo (Km)
-muA = 1.33e-7; % Gravatational parameter of Apollo
+% Asteroid: Arwen
+a4 = 173062100; % Semi-major axis of Arwen' orbit in km (converted from AU)
+e4 = 0.430500436; % Eccentricity of Arwen' orbit
+i4 = 0.339932823; % Inclination of Arwen' orbit in degrees
+Omega4 = 35.92947393; % Longitude of ascending node of Arwen' orbit in degrees
+omega4 = 273.555604; % Argument of periapsis of Arwen' orbit in degrees
+M4 = 189.3528782; % Mean anomaly of Arwen' orbit in degrees
+rA = 2.2; % Radius of initial orbit around Arwen (Km)
+muA = 1.88E-10; % Gravitational parameter of Arwen (assumed similar to Arwen for now)
 
 %% Orbit Definition
 % Orbital velocity for circular orbit
@@ -32,7 +32,7 @@ v_orbit = sqrt(2*muA*1000 / rA*1000);
 T = 2 * pi * sqrt(rA^3 / muA);
 
 % Output results
-fprintf('Altitude: %.2f Km\n', rA - 0.775);
+fprintf('Altitude: %.2f Km\n', rA - 0.2);
 fprintf('Orbital Velocity: %.2f m/s\n', v_orbit);
 fprintf('Orbital Period: %.2f minutes', T / 60);
 fprintf(' (%.2f hours)\n\n', T / 60 / 60);
@@ -46,15 +46,20 @@ rv = E2C(orb, muS); % E2C converts elements to cartesian
 orb_elements = C2E(rv, muS); % C2E converts cartesian to elements
 
 %% Velocity increments for Hohmann transfer Return Leg
-delta_v1r = abs(sqrt((2*muS/a4)-(2*muS/(a1+a4))) - sqrt(muS/a4)); % Calculating deltav for first Hohmann transfer manouver
-deltavr_escape = abs(sqrt((2*muA/rA)+(delta_v1r^2)) - sqrt((2*muA/rA)-(muA/a4))); % Calculating deltav for Apollo escape
-delta_v2r = abs(sqrt(muS/a1) - sqrt((2*muS/a1) - (2*muS/(a1+a4)))); % Calculating deltav for second Hohmann transfer manouver
-deltavr_capture = abs(sqrt(((2*muE)/rE1) + (delta_v2r^2)) - sqrt(((2*muE)/rE1)-(muE/a1))); % Calculating deltav for Earth capture 
-Totaldvr = abs(deltavr_escape + delta_v2r + deltavr_capture); % Total deltav of the transfer
+%delta_v1r = abs(sqrt((2*muS/a4)-(2*muS/(a1+a4))) - sqrt(muS/a4)); % Calculating deltav for first Hohmann transfer manouver
+%deltavr_escape = abs(sqrt((2*muA/rA)+(delta_v1r^2)) - sqrt((2*muA/rA)-(muA/a4))); % Calculating deltav for Arwen escape
+%delta_v2r = abs(sqrt(muS/a1) - sqrt((2*muS/a1) - (2*muS/(a1+a4)))); % Calculating deltav for second Hohmann transfer manouver
+%deltavr_capture = abs(sqrt(((2*muE)/rE1) + (delta_v2r^2)) - sqrt(((2*muE)/rE1)-(muE/a1))); % Calculating deltav for Earth capture 
+%Totaldvr = abs(deltavr_escape + delta_v2r + deltavr_capture); % Total deltav of the transfer
+
+delta_v1r = ;
+
+deltavr_escape = abs(sqrt((2*muA/rA)+(delta_v1r^2)) - sqrt((2*muA/rA)-(muA/a3))); % Calculating deltav for launch body escape
+Totaldvr = abs(deltavr_escape); % Total deltav of the transfer
 
 % Display results
 fprintf('Delta v1r (km/s): %.4f\n', delta_v1r);
-fprintf('Delta v2r (km/s): %.4f\n', delta_v2r);
+%fprintf('Delta v2r (km/s): %.4f\n', delta_v2r);
 fprintf('Delta vr Total (km/s): %.4f\n', Totaldvr);
 
 % Propellant mass 
@@ -67,21 +72,27 @@ mfuel1 = mi1 - m2; % Mass of Fuel for Return Leg
 fprintf('Propellant Mass Return (kg): %.4f\n\n', mfuel1);
 
 %% Velocity increments for Hohmann transfer 
-delta_v1 = abs(sqrt((2*muS/a1)-(2*muS/(a1+a4))) - sqrt(muS/a1)); % Calculating deltav for first Hohmann transfer manouver
-deltav_escape = abs(sqrt((2*muE/rE)+(delta_v1^2)) - sqrt((muE/rE))); % Calculating deltav for Earth escape
-delta_v2 = abs(sqrt(muS/a4) - sqrt((2*muS/a4) - (2*muS/(a1+a4)))); % Calculating deltav for second Hohmann transfer manouver
-deltav_capture = abs(sqrt(((2*muA)/rA) + (delta_v2^2)) - sqrt(((2*muA)/rA)-(muA/a4))); % Calculating deltav for Apollo capture 
-Totaldv = abs(deltav_escape + delta_v2 + deltav_capture); % Total deltav of the transfer
+%delta_v1 = abs(sqrt((2*muS/a1)-(2*muS/(a1+a4))) - sqrt(muS/a1)); % Calculating deltav for first Hohmann transfer manouver
+%deltav_escape = abs(sqrt((2*muE/rE)+(delta_v1^2)) - sqrt((muE/rE))); % Calculating deltav for Earth escape
+%delta_v2 = abs(sqrt(muS/a4) - sqrt((2*muS/a4) - (2*muS/(a1+a4)))); % Calculating deltav for second Hohmann transfer manouver
+%deltav_capture = abs(sqrt(((2*muA)/rA) + (delta_v2^2)) - sqrt(((2*muA)/rA)-(muA/a4))); % Calculating deltav for Arwen capture 
+%Totaldv = abs(deltav_escape + delta_v2 + deltav_capture); % Total deltav of the transfer
+
+delta_v1 = ;
+
+deltav_escape = abs(sqrt((2*muE/rE)+(delta_v1^2)) - sqrt((muE/rE))); % Calculating deltav for launch body escape
+deltav_capture = abs(sqrt(((2*muA)/rA) + (delta_v1^2)) - sqrt(((2*muA)/rA)-(muA/a3))); % Calculating deltav for target body capture
+Totaldv = abs(deltav_escape + deltav_capture); % Total deltav of the transfer
 
 % Display results
-fprintf('Delta v1 (km/s): %.4f\n', delta_v1);
-fprintf('Delta v2 (km/s): %.4f\n', delta_v2);
+%fprintf('Delta v1 (km/s): %.4f\n', delta_v1);
+%fprintf('Delta v2 (km/s): %.4f\n', delta_v2);
 fprintf('Delta v Total (km/s): %.4f\n', Totaldv);
 
 % Propellant mass
 m3 = 1732; % Spacecraft mass + RL10 Engine - no cargo in Kg
-mi2 = mi1/(exp(-Totaldv/ve)); % Earth to Apollo Total Mass
-mfuel2 = mi2 - m3; % Earth to Apollo Fuel Mass
+mi2 = mi1/(exp(-Totaldv/ve)); % Earth to Arwen Total Mass
+mfuel2 = mi2 - m3; % Earth to Arwen Fuel Mass
 fprintf('Propellant Mass Outgoing (kg): %.4f\n\n', mfuel2);
 
 %Total Mass
@@ -90,12 +101,12 @@ mi_launch = mfuel_launch + m3;
 fprintf('Total Launch Mass (kg): %.4f\n', mi_launch);
 fprintf('Total Fuel Mass (kg): %.4f\n\n', mfuel_launch);
 
-% Adjust periapsis distance for Apollo to a realistic value
-close_approach_distance = 0.35 * AU; % Adjusted periapsis distance of Apollo during close approach
+% Periapsis distance for Arwen
+close_approach_distance = 0.6588247999731117 * AU; % Adjusted periapsis distance of Arwen during close approach
 
 %% Hohmann Transfer Orbit for Close Approach
 
-rp = close_approach_distance; % Periapsis distance of Apollo at close approach
+rp = close_approach_distance; % Periapsis distance of Arwen at close approach
 a2 = (a1 + rp) / 2; % Semi-major axis for the Hohmann transfer orbit
 e2 = (rp - a1) / (rp + a1); % Eccentricity of the transfer orbit
 T_transfer = pi * sqrt(a2^3 / muS); % Time for half of the Hohmann transfer
@@ -113,18 +124,18 @@ theta_Earth_launch = n_Earth * T_transfer; % Angle by which Earth has moved duri
 x_Earth_launch = a1 * cos(theta_Earth_launch);
 y_Earth_launch = a1 * sin(theta_Earth_launch);
 
-%% Solving for True Anomaly at Close Approach for Apollo
-n_Apollo = sqrt(muS / a4^3); % Mean motion of Apollo
-M_Apollo_capture = M4 + n_Apollo * T_transfer; % Mean anomaly after transfer time
+%% Solving for True Anomaly at Close Approach for Arwen
+n_Arwen = sqrt(muS / a4^3); % Mean motion of Arwen
+M_Arwen_capture = M4 + n_Arwen * T_transfer; % Mean anomaly after transfer time
 
 % Solve Kepler's equation for eccentric anomaly (E)
-E_Apollo_capture = M_Apollo_capture; % Initial guess for E
+E_Arwen_capture = M_Arwen_capture; % Initial guess for E
 for iter = 1:1000 % Iterate to solve for E
-    E_Apollo_capture = M_Apollo_capture + e4 * sin(E_Apollo_capture); 
+    E_Arwen_capture = M_Arwen_capture + e4 * sin(E_Arwen_capture); 
 end
 
-% True anomaly (nu) of Apollo at capture
-nu_Apollo_capture = 2 * atan2(sqrt(1 + e4) * sin(E_Apollo_capture / 2), sqrt(1 - e4) * cos(E_Apollo_capture / 2));
+% True anomaly (nu) of Arwen at capture
+nu_Arwen_capture = 2 * atan2(sqrt(1 + e4) * sin(E_Arwen_capture / 2), sqrt(1 - e4) * cos(E_Arwen_capture / 2));
 
 %% Rotating the Transfer Orbit
 % Create a theta array for plotting the transfer orbit
@@ -140,7 +151,7 @@ y_transfer_rotated = x_transfer * sin(theta_Earth_launch) + y_transfer * cos(the
 
 %% Plot
 % Create a theta array for plotting orbits
-theta = linspace(0, 2*pi, 1000);         % For full orbits (Earth and Apollo)
+theta = linspace(0, 2*pi, 1000);         % For full orbits (Earth and Arwen)
 
 % Plot orbits with corrections
 figure;
@@ -158,11 +169,11 @@ y_Earth = a1 * sin(theta); % Y-coordinates for Earth's orbit
 plot(x_Earth, y_Earth, 'b', 'LineWidth', 1.5); % Plot Earth's orbit
 legend_entries{end+1} = 'Earth Orbit';
 
-% Apollo Orbit
-x_Apollo = a4 * (cos(theta) - e4); % X-coordinates for Apollo orbit (correcting for center offset)
-y_Apollo = a4 * sqrt(1 - e4^2) * sin(theta); % Y-coordinates for Apollo orbit
-plot(x_Apollo, y_Apollo, 'r', 'LineWidth', 1.5); % Plot Apollo orbit
-legend_entries{end+1} = 'Apollo Orbit';
+% Arwen Orbit
+x_Arwen = a4 * (cos(theta) - e4); % X-coordinates for Arwen orbit (correcting for center offset)
+y_Arwen = a4 * sqrt(1 - e4^2) * sin(theta); % Y-coordinates for Arwen orbit
+plot(x_Arwen, y_Arwen, 'r', 'LineWidth', 1.5); % Plot Arwen orbit
+legend_entries{end+1} = 'Arwen Orbit';
 
 % Plot the rotated transfer orbit
 plot(x_transfer_rotated, y_transfer_rotated, 'g--', 'LineWidth', 1.5); % Plot transfer orbit
@@ -170,9 +181,9 @@ legend_entries{end+1} = 'Transfer Orbit';
 
 % Plot the positions of the bodies at launch and capture
 plot(x_Earth_launch, y_Earth_launch, 'bo', 'MarkerSize', 8, 'MarkerFaceColor', 'b'); % Earth at launch
-plot(a4 * (cos(nu_Apollo_capture) - e4), a4 * sqrt(1 - e4^2) * sin(nu_Apollo_capture), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r'); % Apollo at capture
+plot(a4 * (cos(nu_Arwen_capture) - e4), a4 * sqrt(1 - e4^2) * sin(nu_Arwen_capture), 'ro', 'MarkerSize', 8, 'MarkerFaceColor', 'r'); % Arwen at capture
 legend_entries{end+1} = 'Earth at Launch';
-legend_entries{end+1} = 'Apollo at Capture';
+legend_entries{end+1} = 'Arwen at Capture';
 
 % Set axis limits to improve visibility and keep the plot centered
 xlim([-1.75 * 10^8, 1.75 * 10^8]); % Set X-axis limits based on the orbit size
@@ -181,7 +192,7 @@ ylim([-1.25 * 10^8, 1.25 * 10^8]); % Set Y-axis limits for better proportion
 % Labels and title
 xlabel('X position (km)');
 ylabel('Y position (km)');
-title('Hohmann Transfer for Apollo Close Approach in 2039');
+title('Hohmann Transfer for Arwen Close Approach in 2039');
 legend(legend_entries, 'Location', 'best');
 hold off;
 
@@ -196,13 +207,13 @@ x_Earth = a1 * cos(2 * pi / t_max * vehicle_time);
 y_Earth = a1 * sin(2 * pi / t_max * vehicle_time);
 z_Earth = zeros(size(vehicle_time)); % Earth is assumed to be in the equatorial plane
 
-% Apollo Position
-x_Apollo = a4 * (cos(2 * pi / t_max * vehicle_time) - e4);
-y_Apollo = a4 * sqrt(1 - e4^2) * sin(2 * pi / t_max * vehicle_time);
-z_Apollo = zeros(size(vehicle_time)); % Apollo is also assumed to be in the equatorial plane
+% Arwen Position
+x_Arwen = a4 * (cos(2 * pi / t_max * vehicle_time) - e4);
+y_Arwen = a4 * sqrt(1 - e4^2) * sin(2 * pi / t_max * vehicle_time);
+z_Arwen = zeros(size(vehicle_time)); % Arwen is also assumed to be in the equatorial plane
 
-% Spacecraft Position (assuming spacecraft is initially at Earth and moves towards Apollo)
-x_spacecraft = linspace(a1, a4, length(vehicle_time)); % Linearly move from Earth's orbit to Apollo's orbit
+% Spacecraft Position (assuming spacecraft is initially at Earth and moves towards Arwen)
+x_spacecraft = linspace(a1, a4, length(vehicle_time)); % Linearly move from Earth's orbit to Arwen's orbit
 y_spacecraft = linspace(0, 0, length(vehicle_time)); % Keep in equatorial plane for simplicity
 z_spacecraft = zeros(size(vehicle_time)); % No inclination change
 
