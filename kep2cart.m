edb4077f9f30f48f657ea8706ded7b1fe1b7e2ea
samirahman1203 <157735830+samirahman1203@users.noreply.github.com>
2024-11-21 15:49:30 +0000

    Update kep2cart.m

--- kep2cart.m
+++ kep2cart.m
@@ -1,58 +1,78 @@
-function [r, v] = kep2cart(kep, mu)
-    % Convert Keplerian elements to Cartesian coordinates
-    % INPUTS:
-    %   kep - [a e i Om om Mo], Keplerian elements
-    %   mu  - Gravitational parameter of the central body
-    % OUTPUTS:
-    %   r   - Position vector [km]
-    %   v   - Velocity vector [km/s]
+function [r, v] = kep2cart(kep,mu)
 
-    % Extract elements
-    a = kep(1); % Semi-major axis
-    e = kep(2); % Eccentricity
-    i = kep(3); % Inclination
-    Om = kep(4); % Longitude of ascending node
-    om = kep(5); % Argument of periapsis
-    Mo = kep(6); % Mean anomaly at epoch
+%   Converts from keplerian orbital elements to Cartesian coordinates.
+%
+%   out = kep2cart(kep, mu)
+%
+%   All units to be consistent each other. Angles in radians.
+%   In case of a parabola, in which e == 1, kep(1) must be the radius of
+%   pericentre, and not the semi-major axis (Inf for a parabola).
+%   Note: In the case of hyperbola, theta must be such that the point is on
+%   the physical leg of the hyperbola (the leg around the attracting mass).
+%  
+%   INPUT
+%               kep(6) = vector of keplerian elements
+%                        [a e i Om om theta]
+%                        angles in [rad]
+%               mu  = planetary gravity constant
+%
+%   OUTPUT
+%               out = State vector in cartesian coordinates (position,
+%                     velocity)
+%
+%  (c) Massimiliano Vasile 2002
+%  Revised by Matteo Ceriotti, 12-02-2007
+%  17-04-2007: Camilla Colombo, Matteo Ceriotti: checked case of the
+%              non-physical leg of the hyperbola and added note.
+%  27-02-2008: Matteo Ceriotti, Daniel Novak: added the parabolic case, in
+%              which kep(1)=rp.
+%
+% ------------------------- - SpaceART Toolbox - --------------------------
 
-    % Solve Kepler's Equation for Eccentric Anomaly using Newton-Raphson method
-    E = Mo; % Initial guess for Eccentric Anomaly
-    tol = 1e-8; % Convergence tolerance
-    max_iter = 100; % Maximum number of iterations
+e   = kep(2);
+i   = kep(3);
+Om  = kep(4);
+om  = kep(5);
+tho = kep(6);
 
-    for iter = 1:max_iter
-        f = E - e * sin(E) - Mo; % Kepler's equation
-        f_prime = 1 - e * cos(E); % Derivative of Kepler's equation
-        E_new = E - f / f_prime; % Update using Newton-Raphson method
-        if abs(E_new - E) < tol
-            E = E_new;
-            break; % Converged
-        end
-        E = E_new;
-    end
+% Rotation matrix
+R(1,1)=cos(om)*cos(Om)-sin(om)*cos(i)*sin(Om);
+R(2,1)=cos(om)*sin(Om)+sin(om)*cos(i)*cos(Om);
+R(3,1)=sin(om)*sin(i);
 
-    % Compute true anomaly (nu)
-    nu = 2 * atan2(sqrt(1 + e) * sin(E / 2), sqrt(1 - e) * cos(E / 2));
+R(1,2)=-sin(om)*cos(Om)-cos(om)*cos(i)*sin(Om);
+R(2,2)=-sin(om)*sin(Om)+cos(om)*cos(i)*cos(Om);
+R(3,2)=cos(om)*sin(i);
 
-    % Compute distance (r_mag)
-    r_mag = a * (1 - e * cos(E));
+R(1,3)=sin(i)*sin(Om);
+R(2,3)=-sin(i)*cos(Om);
+R(3,3)=cos(i);
 
-    % Position in the orbital plane (perifocal coordinates)
-    r_perifocal = [r_mag * cos(nu); r_mag * sin(nu); 0];
-
-    % Velocity in the orbital plane (perifocal coordinates)
-    h = sqrt(mu * a * (1 - e^2)); % Specific angular momentum
-    v_perifocal = (mu / h) * [-sin(nu); e + cos(nu); 0];
-
-    % Rotation matrices to convert from perifocal to inertial frame
-    R3_W = [cos(Om), -sin(Om), 0; sin(Om), cos(Om), 0; 0, 0, 1]; % Rotation about z by Om
-    R1_i = [1, 0, 0; 0, cos(i), -sin(i); 0, sin(i), cos(i)]; % Rotation about x by i
-    R3_w = [cos(om), -sin(om), 0; sin(om), cos(om), 0; 0, 0, 1]; % Rotation about z by om
-
-    % Combined rotation matrix
-    Q_pX = R3_W * R1_i * R3_w;
-
-    % Position and velocity in the inertial frame
-    r = Q_pX * r_perifocal; % Position in inertial frame
-    v = Q_pX * v_perifocal; % Velocity in inertial frame
+%     In plane     Parameters
+if e==1 % Parabola
+    p=2*kep(1); % In the case of a parabola, kep(1) is rp
+else
+    p = kep(1)*(1-e^2);
 end
+
+r=p/(1+e*cos(tho));
+xp=r*cos(tho);
+yp=r*sin(tho);
+wom_dot = sqrt(mu*p)/r^2;
+r_dot   = sqrt(mu/p)*e*sin(tho);
+vxp=r_dot*cos(tho)-r*sin(tho)*wom_dot;
+vyp=r_dot*sin(tho)+r*cos(tho)*wom_dot;
+
+% 3D cartesian vector
+out(1)=R(1,1)*xp+R(1,2)*yp;
+out(2)=R(2,1)*xp+R(2,2)*yp;
+out(3)=R(3,1)*xp+R(3,2)*yp;
+
+out(4)=R(1,1)*vxp+R(1,2)*vyp;
+out(5)=R(2,1)*vxp+R(2,2)*vyp;
+out(6)=R(3,1)*vxp+R(3,2)*vyp;
+
+r = out(1:3);
+v = out(4:6);
+
+return
